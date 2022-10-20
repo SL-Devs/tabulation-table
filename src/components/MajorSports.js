0114aa425d52e8dc1e08e7e6d0d3c56a10b9117d
Jaycon-Jude-Madrid <jayconmadrid46@gmail.com>
2022-10-20 23:12:32 +0800

    Added Sports

--- src/components/MajorSports.js
+++ src/components/MajorSports.js
@@ -4,185 +4,104 @@ import Box from "@mui/material/Box";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { Typography } from "@mui/material";
 import { db } from "../Firebase-config";
-import { child, get, onValue, ref } from "firebase/database";
-
-const columns = [
-  { field: "id", headerName: `Course`, width: 200, type: "string" },
-
-  {
-    field: "Basketball",
-    headerName: "Basketball",
-    type: "number",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "BasketBallMen",
-    headerName: "Men",
-    type: "number",
-    width: 50,
-    editable: true,
-  },
-  {
-    field: "BasketBallWomen",
-    headerName: "Women",
-    type: "number",
-    width: 70,
-    editable: true,
-  },
-
-  {
-    field: "Volleyball",
-    headerName: "Volleyball",
-    type: "number",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "VolleyballMen",
-    headerName: "Men",
-    type: "number",
-    width: 50,
-    editable: true,
-  },
-  {
-    field: "VolleyballWomen",
-    headerName: "Women",
-    type: "number",
-    width: 70,
-    editable: true,
-  },
-  {
-    field: "MrandMsSTI",
-    headerName: "Mr and Ms STI",
-    type: "number",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "MRandMSMen",
-    headerName: "Men",
-    type: "number",
-    width: 50,
-    editable: true,
-  },
-  {
-    field: "MRandMSWomen",
-    headerName: "Women",
-    type: "number",
-    width: 70,
-    editable: true,
-  },
-  {
-    field: "Cheerdance",
-    headerName: "Cheerdance",
-    type: "number",
-    width: 160,
-    editable: true,
-  },
-  {
-    field: "BannerRaising",
-    headerName: "Banner Raising",
-    type: "number",
-    width: 160,
-    editable: true,
-  },
-  {
-    field: "HatawSayaw",
-    headerName: "Hataw Sayaw",
-    type: "number",
-    width: 160,
-    editable: true,
-  },
-  {
-    field: "BattleOfTheBands",
-    headerName: "Battle Of The Bands",
-    type: "number",
-    width: 200,
-    editable: true,
-  },
-  {
-    field: "PopIdol",
-    headerName: "Pop Idol",
-    type: "number",
-    width: 160,
-    editable: true,
-  },
-  {
-    field: "Total",
-    headerName: "Total Score",
-    type: "number",
-    width: 160,
-    editable: true,
-  },
-];
-
+import { onValue, ref } from "firebase/database";
+import { MajorColumns as columns } from "../utils/columns";
 const MajorSports = () => {
   const [data, setData] = useState(null);
 
   const getdata = () => {
-    const dbRef = ref(db);
-    get(child(dbRef, "Major Sports"))
-      .then((snapshot) => {
-        if (snapshot.exists()) {
-          setData(snapshot.val());
-        } else {
-          console.log("No data available");
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    const starCountRef = ref(db, "Major");
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      setData(data);
+    });
   };
 
   useEffect(() => {
     getdata();
   }, []);
+
   const rows = [
     {
       id: "IT-SPHINX",
-      Cheerdance: 0,
-      BasketBallMen: 0,
-      BasketBallWomen: 0,
-      MRandMSMen: 0,
-      MRandMSWomen: 0,
-      VolleyballMen: 20,
-      VolleyballWomen: 20,
-      BannerRaising: 35,
-      HatawSayaw: 20,
-      BattleOfTheBands: 35,
-      PopIdol: 35,
-      Total: 250,
+      Cheerdance: data && data.Cheerdance.sphinx,
+      BasketBallMen: data && data["Basketball(Men)"].sphinx,
+      BasketBallWomen: data && data["Basketball(Women)"].sphinx,
+      MRandMSMen: data && data["Mr STI 2022"].sphinx,
+      MRandMSWomen: data && data["Ms STI 2022"].sphinx,
+      VolleyballMen: data && data["Volleyball(Men)"].sphinx,
+      VolleyballWomen: data && data["Volleyball(Women)"].sphinx,
+      BannerRaising: data && data["Banner Raising"].sphinx,
+      HatawSayaw: data && data["Hataw Sayaw"].sphinx,
+      BattleOfTheBands: data && data["Battle of the Bands"].sphinx,
+      PopIdol: data && data["Pop Idol"].sphinx,
+      Total:
+        data &&
+        parseInt(data.Cheerdance.sphinx) +
+          parseInt(data["Basketball(Men)"].sphinx) +
+          parseInt(data["Basketball(Women)"].sphinx) +
+          parseInt(data["Mr STI 2022"].sphinx) +
+          parseInt(data["Ms STI 2022"].sphinx) +
+          parseInt(data["Volleyball(Men)"].sphinx) +
+          parseInt(data["Volleyball(Women)"].sphinx) +
+          parseInt(data["Banner Raising"].sphinx) +
+          parseInt(data["Hataw Sayaw"].sphinx) +
+          parseInt(data["Battle of the Bands"].sphinx) +
+          parseInt(data["Pop Idol"].sphinx),
     },
     {
       id: "HM-PHOENIX",
-      Cheerdance: 60,
-      BasketBallMen: 20,
-      BasketBallWomen: 20,
-      MRandMSMen: 20,
-      MRandMSWomen: 35,
-      VolleyballMen: 20,
-      VolleyballWomen: 20,
-      BannerRaising: 35,
-      HatawSayaw: 20,
-      BattleOfTheBands: 35,
-      PopIdol: 35,
-      Total: 250,
+      Cheerdance: data && data.Cheerdance.phoenix,
+      BasketBallMen: data && data["Basketball(Men)"].phoenix,
+      BasketBallWomen: data && data["Basketball(Women)"].phoenix,
+      MRandMSMen: data && data["Mr STI 2022"].phoenix,
+      MRandMSWomen: data && data["Ms STI 2022"].phoenix,
+      VolleyballMen: data && data["Volleyball(Men)"].phoenix,
+      VolleyballWomen: data && data["Volleyball(Women)"].phoenix,
+      BannerRaising: data && data["Banner Raising"].phoenix,
+      HatawSayaw: data && data["Hataw Sayaw"].phoenix,
+      BattleOfTheBands: data && data["Battle of the Bands"].phoenix,
+      PopIdol: data && data["Pop Idol"].phoenix,
+      Total:
+        data &&
+        parseInt(data.Cheerdance.phoenix) +
+          parseInt(data["Basketball(Men)"].phoenix) +
+          parseInt(data["Basketball(Women)"].phoenix) +
+          parseInt(data["Mr STI 2022"].phoenix) +
+          parseInt(data["Ms STI 2022"].phoenix) +
+          parseInt(data["Volleyball(Men)"].phoenix) +
+          parseInt(data["Volleyball(Women)"].phoenix) +
+          parseInt(data["Banner Raising"].phoenix) +
+          parseInt(data["Hataw Sayaw"].phoenix) +
+          parseInt(data["Battle of the Bands"].phoenix) +
+          parseInt(data["Pop Idol"].phoenix),
     },
     {
       id: "BA-GRIFFIN",
-      Cheerdance: 60,
-      BasketBallMen: 20,
-      BasketBallWomen: 20,
-      MRandMSMen: 20,
-      MRandMSWomen: 35,
-      VolleyballMen: 20,
-      VolleyballWomen: 20,
-      BannerRaising: 35,
-      HatawSayaw: 20,
-      BattleOfTheBands: 35,
-      PopIdol: 35,
-      Total: 250,
+      Cheerdance: data && data.Cheerdance.griffin,
+      BasketBallMen: data && data["Basketball(Men)"].griffin,
+      BasketBallWomen: data && data["Basketball(Women)"].griffin,
+      MRandMSMen: data && data["Mr STI 2022"].griffin,
+      MRandMSWomen: data && data["Ms STI 2022"].griffin,
+      VolleyballMen: data && data["Volleyball(Men)"].griffin,
+      VolleyballWomen: data && data["Volleyball(Women)"].griffin,
+      BannerRaising: data && data["Banner Raising"].griffin,
+      HatawSayaw: data && data["Hataw Sayaw"].griffin,
+      BattleOfTheBands: data && data["Battle of the Bands"].griffin,
+      PopIdol: data && data["Pop Idol"].griffin,
+      Total:
+        data &&
+        parseInt(data.Cheerdance.griffin) +
+          parseInt(data["Basketball(Men)"].griffin) +
+          parseInt(data["Basketball(Women)"].griffin) +
+          parseInt(data["Mr STI 2022"].griffin) +
+          parseInt(data["Ms STI 2022"].griffin) +
+          parseInt(data["Volleyball(Men)"].griffin) +
+          parseInt(data["Volleyball(Women)"].griffin) +
+          parseInt(data["Banner Raising"].griffin) +
+          parseInt(data["Hataw Sayaw"].griffin) +
+          parseInt(data["Battle of the Bands"].griffin) +
+          parseInt(data["Pop Idol"].griffin),
     },
   ];
 
@@ -211,7 +130,7 @@ const MajorSports = () => {
             textTransform: "uppercase",
           }}
         >
-          Major Sports
+          MAJOR EVENTS & MAJOR SPORTS
         </Typography>
       </Box>
 
@@ -219,7 +138,7 @@ const MajorSports = () => {
         sx={{
           fontSize: "15px",
           "& .css-1j9kmqg-MuiDataGrid-toolbarContainer": {
-            padding: "20px",
+            padding: "20px !important",
           },
         }}
         rows={rows}
--- src/components/MinorSports.js
+++ src/components/MinorSports.js
@@ -1,206 +1,147 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { Typography } from "@mui/material";
-
-const columns = [
-  { field: "id", headerName: `Course`, width: 200, type: "string" },
-
-  {
-    field: "Badminton",
-    headerName: "Badminton",
-    type: "number",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "BadmintonMen",
-    headerName: "Men",
-    type: "number",
-    width: 50,
-    editable: true,
-  },
-  {
-    field: "BadmintonWomen",
-    headerName: "Women",
-    type: "number",
-    width: 70,
-    editable: true,
-  },
-  {
-    field: "BadmintonDoubles",
-    headerName: "Doubles",
-    type: "number",
-    width: 70,
-    editable: true,
-  },
-  {
-    field: "TableTennis",
-    headerName: "Table Tennis",
-    type: "number",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "TableTennisMen",
-    headerName: "Men",
-    type: "number",
-    width: 50,
-    editable: true,
-  },
-  {
-    field: "TableTennisWomen",
-    headerName: "Women",
-    type: "number",
-    width: 70,
-    editable: true,
-  },
-  {
-    field: "Chess",
-    headerName: "Chess",
-    type: "number",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "ChessMen",
-    headerName: "Men",
-    type: "number",
-    width: 50,
-    editable: true,
-  },
-  {
-    field: "ChessWomen",
-    headerName: "Women",
-    type: "number",
-    width: 70,
-    editable: true,
-  },
-  {
-    field: "Scrabble",
-    headerName: "Scrabble",
-    type: "number",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "ScrabbleMen",
-    headerName: "Men",
-    type: "number",
-    width: 50,
-    editable: true,
-  },
-  {
-    field: "ScrabbleWomen",
-    headerName: "Women",
-    type: "number",
-    width: 70,
-    editable: true,
-  },
-  {
-    field: "TugOfWar",
-    headerName: "Tug of War",
-    type: "number",
-    width: 160,
-    editable: true,
-  },
-  {
-    field: "TugOfWarMen",
-    headerName: "Men",
-    type: "number",
-    width: 50,
-    editable: true,
-  },
-  {
-    field: "TugOfWarWomen",
-    headerName: "Women",
-    type: "number",
-    width: 70,
-    editable: true,
-  },
-  {
-    field: "Dart",
-    headerName: "Dart",
-    type: "number",
-    width: 150,
-    editable: true,
-  },
-  {
-    field: "DartMen",
-    headerName: "Men",
-    type: "number",
-    width: 50,
-    editable: true,
-  },
-  {
-    field: "DartWomen",
-    headerName: "Women",
-    type: "number",
-    width: 70,
-    editable: true,
-  },
-  {
-    field: "Sackrace",
-    headerName: "Sackrace",
-    type: "number",
-    width: 200,
-    editable: true,
-  },
-  {
-    field: "Total",
-    headerName: "Total Score",
-    type: "number",
-    width: 160,
-    editable: true,
-  },
-];
+import { db } from "../Firebase-config";
+import { child, get, onValue, ref } from "firebase/database";
+import { MinorColumns as columns } from "../utils/columns";
 
 const MinorSports = () => {
+  const [data, setData] = useState(null);
+
+  const getdata = () => {
+    const starCountRef = ref(db, "Minor");
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      setData(data);
+    });
+  };
+
+  useEffect(() => {
+    getdata();
+  }, []);
+
+  console.log(data);
   const rows = [
     {
       id: "IT-SPHINX",
-      Cheerdance: 60,
-      BasketBallMen: 20,
-      BasketBallWomen: 20,
-      MRandMSMen: 20,
-      MRandMSWomen: 35,
-      VolleyballMen: 20,
-      VolleyballWomen: 20,
-      BannerRaising: 35,
-      HatawSayaw: 20,
-      BattleOfTheBands: 35,
-      PopIdol: 35,
-      Total: 250,
+      BadmintonMen: data && data["Badminton(Men)"].sphinx,
+      BadmintonWomen: data && data["Badminton(Women)"].sphinx,
+      BadmintonDoubles: data && data["Badminton(Doubles)"].sphinx,
+      TableTennisMen: data && data["Table Tennis(Men)"].sphinx,
+      TableTennisWomen: data && data["Table Tennis(Women)"].sphinx,
+      TableTennisDoubles: data && data["Table Tennis(Doubles)"].sphinx,
+      ChessMen: data && data["Chess(Men)"].sphinx,
+      ChessWomen: data && data["Chess(Women)"].sphinx,
+      ScrabbleMen: data && data["Scrabble(Men)"].sphinx,
+      ScrabbleWomen: data && data["Scrabble(Women)"].sphinx,
+      TugOfWarMen: data && data["Tug of War(Men)"].sphinx,
+      TugOfWarWomen: data && data["Tug of War(Women)"].sphinx,
+      DartMen: data && data["Dart(Men)"].sphinx,
+      DartWomen: data && data["Dart(Women)"].sphinx,
+      Sackrace: data && data["Sack Race"].sphinx,
+      SepakTakraw: 10,
+      PosterMaking: 10,
+      Patintero: 10,
+      RunningMan: 10,
+      WordFactoryScribble: 10,
+      Total:
+        data &&
+        parseInt(data["Badminton(Men)"].sphinx) +
+          parseInt(data["Badminton(Women)"].sphinx) +
+          parseInt(data["Badminton(Doubles)"].sphinx) +
+          parseInt(data["Table Tennis(Men)"].sphinx) +
+          parseInt(data["Table Tennis(Women)"].sphinx) +
+          parseInt(data["Table Tennis(Doubles)"].sphinx) +
+          parseInt(data["Chess(Men)"].sphinx) +
+          parseInt(data["Chess(Women)"].sphinx) +
+          parseInt(data["Scrabble(Women)"].sphinx) +
+          parseInt(data["Scrabble(Women)"].sphinx) +
+          parseInt(data["Tug of War(Men)"].sphinx) +
+          parseInt(data["Tug of War(Women)"].sphinx) +
+          parseInt(data["Dart(Men)"].sphinx) +
+          parseInt(data["Dart(Women)"].sphinx) +
+          parseInt(data["Sack Race"].sphinx),
     },
     {
       id: "HM-PHOENIX",
-      Cheerdance: 60,
-      BasketBallMen: 20,
-      BasketBallWomen: 20,
-      MRandMSMen: 20,
-      MRandMSWomen: 35,
-      VolleyballMen: 20,
-      VolleyballWomen: 20,
-      BannerRaising: 35,
-      HatawSayaw: 20,
-      BattleOfTheBands: 35,
-      PopIdol: 35,
-      Total: 250,
+      BadmintonMen: data && data["Badminton(Men)"].phoenix,
+      BadmintonWomen: data && data["Badminton(Women)"].phoenix,
+      BadmintonDoubles: data && data["Badminton(Doubles)"].phoenix,
+      TableTennisMen: data && data["Table Tennis(Men)"].phoenix,
+      TableTennisWomen: data && data["Table Tennis(Women)"].phoenix,
+      TableTennisDoubles: data && data["Table Tennis(Doubles)"].phoenix,
+      ChessMen: data && data["Chess(Men)"].phoenix,
+      ChessWomen: data && data["Chess(Women)"].phoenix,
+      ScrabbleMen: data && data["Scrabble(Men)"].phoenix,
+      ScrabbleWomen: data && data["Scrabble(Women)"].phoenix,
+      TugOfWarMen: data && data["Tug of War(Men)"].phoenix,
+      TugOfWarWomen: data && data["Tug of War(Women)"].phoenix,
+      DartMen: data && data["Dart(Men)"].phoenix,
+      DartWomen: data && data["Dart(Women)"].phoenix,
+      Sackrace: data && data["Sack Race"].phoenix,
+      SepakTakraw: 10,
+      PosterMaking: 10,
+      Patintero: 10,
+      RunningMan: 10,
+      WordFactoryScribble: 10,
+      Total:
+        data &&
+        parseInt(data["Badminton(Men)"].phoenix) +
+          parseInt(data["Badminton(Women)"].phoenix) +
+          parseInt(data["Badminton(Doubles)"].phoenix) +
+          parseInt(data["Table Tennis(Men)"].phoenix) +
+          parseInt(data["Table Tennis(Women)"].phoenix) +
+          parseInt(data["Table Tennis(Doubles)"].phoenix) +
+          parseInt(data["Chess(Men)"].phoenix) +
+          parseInt(data["Chess(Women)"].phoenix) +
+          parseInt(data["Scrabble(Women)"].phoenix) +
+          parseInt(data["Scrabble(Women)"].phoenix) +
+          parseInt(data["Tug of War(Men)"].phoenix) +
+          parseInt(data["Tug of War(Women)"].phoenix) +
+          parseInt(data["Dart(Men)"].phoenix) +
+          parseInt(data["Dart(Women)"].phoenix) +
+          parseInt(data["Sack Race"].phoenix),
     },
     {
       id: "BA-GRIFFIN",
-      Cheerdance: 60,
-      BasketBallMen: 20,
-      BasketBallWomen: 20,
-      MRandMSMen: 20,
-      MRandMSWomen: 35,
-      VolleyballMen: 20,
-      VolleyballWomen: 20,
-      BannerRaising: 35,
-      HatawSayaw: 20,
-      BattleOfTheBands: 35,
-      PopIdol: 35,
-      Total: 250,
+      BadmintonMen: data && data["Badminton(Men)"].griffin,
+      BadmintonWomen: data && data["Badminton(Women)"].griffin,
+      BadmintonDoubles: data && data["Badminton(Doubles)"].griffin,
+      TableTennisMen: data && data["Table Tennis(Men)"].griffin,
+      TableTennisWomen: data && data["Table Tennis(Women)"].griffin,
+      TableTennisDoubles: data && data["Table Tennis(Doubles)"].griffin,
+      ChessMen: data && data["Chess(Men)"].griffin,
+      ChessWomen: data && data["Chess(Women)"].griffin,
+      ScrabbleMen: data && data["Scrabble(Men)"].griffin,
+      ScrabbleWomen: data && data["Scrabble(Women)"].griffin,
+      TugOfWarMen: data && data["Tug of War(Men)"].griffin,
+      TugOfWarWomen: data && data["Tug of War(Women)"].griffin,
+      DartMen: data && data["Dart(Men)"].griffin,
+      DartWomen: data && data["Dart(Women)"].griffin,
+      Sackrace: data && data["Sack Race"].griffin,
+      SepakTakraw: 10,
+      PosterMaking: 10,
+      Patintero: 10,
+      RunningMan: 10,
+      WordFactoryScribble: 10,
+      Total:
+        data &&
+        parseInt(data["Badminton(Men)"].griffin) +
+          parseInt(data["Badminton(Women)"].griffin) +
+          parseInt(data["Badminton(Doubles)"].griffin) +
+          parseInt(data["Table Tennis(Men)"].griffin) +
+          parseInt(data["Table Tennis(Women)"].griffin) +
+          parseInt(data["Table Tennis(Doubles)"].griffin) +
+          parseInt(data["Chess(Men)"].griffin) +
+          parseInt(data["Chess(Women)"].griffin) +
+          parseInt(data["Scrabble(Women)"].griffin) +
+          parseInt(data["Scrabble(Women)"].griffin) +
+          parseInt(data["Tug of War(Men)"].griffin) +
+          parseInt(data["Tug of War(Women)"].griffin) +
+          parseInt(data["Dart(Men)"].griffin) +
+          parseInt(data["Dart(Women)"].griffin) +
+          parseInt(data["Sack Race"].griffin),
     },
   ];
 
@@ -212,7 +153,7 @@ const MinorSports = () => {
           justifyContent: "center",
           padding: "20px",
           backgroundColor: "rgb(0, 114, 187)",
-          color: "yellow",
+          color: "white",
         }}
       >
         <Typography
@@ -222,7 +163,7 @@ const MinorSports = () => {
             textTransform: "uppercase",
           }}
         >
-          Minor Sports
+          MINOR SPORTS
         </Typography>
       </Box>
 
@@ -230,7 +171,7 @@ const MinorSports = () => {
         sx={{
           fontSize: "15px",
           "& .css-1j9kmqg-MuiDataGrid-toolbarContainer": {
-            padding: "20px",
+            padding: "20px !important",
           },
         }}
         rows={rows}
